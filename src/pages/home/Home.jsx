4ad68b2099fb3971cc020b86ddb8c18555b935f7
Kailash Kunwar <kailashkunwar10@gmail.com>
2025-06-20 22:32:33 -0700

    used another branch

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,1 +1,27 @@
 
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prev) => (prev > 0 ? prev - 1 : prev));
+  };
+
+  console.log("re-render");
+
+  return (
+    <div >
+      <h1>
+        {count} - {count % 2 === 0 ? "even" : "odd"}
+      </h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <br />
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
