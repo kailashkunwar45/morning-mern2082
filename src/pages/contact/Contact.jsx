588f9bd9bd6a0f005e3dd581b9089dbdab47db3f
Kailash Kunwar <kailashkunwar10@gmail.com>
2025-06-24 19:24:59 -0700

    hgdgf

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,31 @@
-import { Avatar } from '@material-tailwind/react'
-import React from 'react'
+import React, { useState } from 'react';
+import { faker } from '@faker-js/faker';
+import { Button, Card, List, ListItem } from '@material-tailwind/react';
 
-export default function Contact() {
+export default function Home() {
+  const [isRed, setChange] = useState(false);
+  const [image, setImage] = useState('');
+  const [email, setEmail] = useState('');
+
+  const toggleColor = () => setChange(prev => !prev);
+  const generateEmail = () => setEmail(faker.internet.email());
+  const generateImage = () => setImage(faker.image.avatar());
+
   return (
-    <div>
+    <div className="flex flex-col items-center gap-4 p-6">
+      <Button onClick={toggleColor} className={`text-white ${isRed ? 'bg-red-600' : 'bg-black'}`}>
+        Change Color
+      </Button>
 
+      <Button onClick={generateEmail}>Generate Email</Button>
+      <Card className="w-96">
+        <List>
+          <ListItem>{email}</ListItem>
+        </List>
+      </Card>
+
+      <Button onClick={generateImage}>Generate Image</Button>
+      {image && <img className="h-[400px] w-[400px] object-cover" src={image} alt="Avatar" />}
     </div>
-  )
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,57 +1,6 @@
-import { faker } from '@faker-js/faker';
-import { Button } from '@material-tailwind/react';
-import {
-  List,
-  ListItem,
-  ListItemSuffix,
-  Chip,
-  Card,
-} from "@material-tailwind/react";
-import { useState } from 'react';
-
-export default function Home() {
-
-  const [data, setData] = useState([]);
-
-  const handleClick = () => {
-    setData((prev) => [...prev, faker.science.chemicalElement()]);
-  }
-
-  const handleDelete = (indexToDelete) => {
-    setData((prev) => prev.filter((_, index) => index !== indexToDelete));
-  }
-
-  return (
-    <div className='p-5'>
-
-      <Button onClick={handleClick} className='cursor-pointer'> Add Fake Data</Button>
-
-      {data.length > 0 && <Card className="w-96 mt-6">
-        <List>
-          {data.map((chemical, index) => {
-            return <ListItem key={index}>
-              {chemical.name}
-              <ListItemSuffix className='flex gap-5'>
-                <Chip
-                  value={chemical.symbol}
-                  variant="ghost"
-                  size="sm"
-                  className="rounded-full"
-                />
-                <Button onClick={() => handleDelete(index)} size='sm' className='text-[10px]' color='red'>Delete</Button>
-
-              </ListItemSuffix>
-            </ListItem>
-          })}
 
 
 
-        </List>
-      </Card>
-      }
 
 
-    </div>
-  )
-}
 
