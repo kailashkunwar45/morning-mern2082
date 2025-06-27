a0e83197cde3ab02bfc7d12721825b5cdf202fd9
Kailash Kunwar <kailashkunwar10@gmail.com>
2025-06-26 17:00:40 -0700

    ygysdfov

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,40 @@
 
 
+import React, { useEffect, useRef, useState } from 'react'
+
+
+
+// const m = axios.get("https://jsonplaceholder.typicode.com/posts")
+
+export default function Home() {
+
+  const resultref = useRef('loading...');
+  const [render, setrender]
+    = useState(false);
+
+  useEffect(() => {
+    const m = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Rajesh Hamal");
+      }, 2000)
+    });
+  });
+
+  m.then((val) => {
+    console.log(`Sucess : ${val}`);
+  }).catch((err) => {
+    console.log(`rejected : ${err}`);
+  })
+  // .finally(() => {
+  //   console.log('hello')
+  // })
+
+  return (
+    <div>
+      {resultref.current}
+    </div>
+  )
+}
 
 
 
