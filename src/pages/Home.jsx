f010d6c59f47a11933fa5793a92acf8f5ded3641
Kailash Kunwar <kailashkunwar10@gmail.com>
2025-06-28 19:34:08 -0700

    hgfs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+
+
+
+import axios from "axios"
+
 
 export default function Home() {
+
+  const m = axios.get('https://jsonplaceholder.typicode.com/posts');
+
+  console.log(m);
+
+
   return (
     <div>
 
     </div>
   )
 }
+
